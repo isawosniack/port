5cfeeb1d136f19e8535b922991dbb6fa359f299b
GUEWK62 <extern.isabella.fernanda.wosniack@cariad.technology>
2023-11-21 08:28:41 +0100

    working on my database

--- server.py
+++ server.py
@@ -35,18 +35,21 @@ def write_to_file(data):
         file = database.write(f'\n{email}, {subject}, {message}')
         
 def write_to_csv(data):
-    with open('database.csv',mode='a') as database2:
+    with open('database.csv',newline='', mode='a') as database2:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        csv_writer = csv.writer(database2, delimiter=',', newline='', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email,subject,message])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
-    if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_csv(data)
-        return redirect('/thankyou.html')
+    try:
+        if request.method == 'POST':
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+    except:
+        return 'Ops! Something went wrong'
     else:
         return 'something went wrong'
